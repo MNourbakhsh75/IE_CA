ad64b2309bb2b2004a69a34df4de1bb7084b0685
Mehrdad Nourbakhsh <m.nourbakhsh75@gmail.com>
2019-03-18 19:15:33 +0330

    add CORS filter

---
+++ src/main/java/Filters/CORSFilter.java
@@ -0,0 +1,33 @@
+package Filters;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter(filterName = "filters.CORSFilter", urlPatterns = {"/*"})
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("CORS!");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Credentials", "true");
+        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS,DELETE");
+        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
