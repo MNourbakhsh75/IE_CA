b1e576f787c6f5db0b131bffb356cc43725c6cfb
Mehrdad Nourbakhsh <m.nourbakhsh75@gmail.com>
2019-05-31 23:29:29 +0430

    -Fixed some bug

--- src/main/java/JobOonja/DataLayer/DBConnectionPool/ConnectionPool.java
+++ src/main/java/JobOonja/DataLayer/DBConnectionPool/ConnectionPool.java
@@ -33,24 +33,7 @@ public class ConnectionPool {
         }
         System.out.println("test database5");
         return DriverManager.getConnection("jdbc:mysql://mysql-database:3306/test?allowPublicKeyRetrieval=true&useSSL=false","root","root");
-//        Connection c = null;
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//        }
-//        catch (ClassNotFoundException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            String url = "jdbc:mysql://localhost:3333/joboonjadb";
-//            c = DriverManager.getConnection(url, "root", "");
-//
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        return c;
+
     }
 
     private ConnectionPool(){ }
